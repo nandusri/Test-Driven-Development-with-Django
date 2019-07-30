10a60daf4668d122b5090b310f5b50a4e9b66c29
kasu.nandu@gmail.com <kasu.nandu@gmail.com>
2019-07-30 17:43:49 +0530

    retrive,delete,update and it's test cases added

--- apiapp/tests.py
+++ apiapp/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import List
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -37,3 +37,32 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_list(self):
         """Test the api"""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_list(self):
+        """Test the api can get a given list."""
+        bucketlist = List.objects.get()
+        response = self.client.get(
+            reverse('details',
+            kwargs={'pk': bucketlist.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_list(self):
+        """Test the api can update a given bucketlist."""
+        change_bucketlist = {'name': 'Something new'}
+        res = self.client.put(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            change_bucketlist, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_list(self):
+        """Test the api can delete a bucketlist."""
+        bucketlist = List.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
--- apiapp/urls.py
+++ apiapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView
+from .views import CreateView,DetailsView
 
 urlpatterns = {
     path('lists/', CreateView.as_view(), name="create"),
+    path('lists/<pk>',
+        DetailsView.as_view(), name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
